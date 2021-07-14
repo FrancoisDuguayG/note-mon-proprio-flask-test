3f142fc9c9f145b9c0827e058f7ef7afe9528e27
FrancoisDuguayG <francois.duguay-giguere@ingeno.ca>
2021-07-14 16:39:16 -0400

    Modified top-bar

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
-bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def hello_world():
     return render_template("main.html")
+
+
+@app.route('/index')
+def hello_index():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
